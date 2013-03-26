89ff9913dd2081eb54d9b3aecc9555bf4cba9d26
Patti <pj.97211@gmail.com>
2013-03-26 10:02:23 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewRailsCardApp::Application.config.secret_token = 'f7d20b232679078e8ab5a664667833dde7ebe5419a35d18665e6cf4dbd0940711c17e555abeae8cf9d9cde3f9deea57f82317889e6e055f83ce8b54af042fccf'
